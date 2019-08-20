ec139258837b21419fc9eb806ec4c9559cdb84ce
Zanetti <znti.rafael@gmail.com>
2019-08-20 17:10:36 -0300

    Include code sample to handle what is returned from Decode

--- app/src/main/java/br/com/dcod/modules/gss/ResultActivity.java
+++ app/src/main/java/br/com/dcod/modules/gss/ResultActivity.java
@@ -2,7 +2,9 @@ package br.com.dcod.modules.gss;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent reportIntent = getIntent();
+
+        if(reportIntent != null) {
+            int idServico = reportIntent.getIntExtra("idServico", 0);
+            int idContrato = reportIntent.getIntExtra("idContrato", 0);
+            int qtdeAuditorias = reportIntent.getIntExtra("qtdeAuditorias", 0);
+            int qtdeAuditoriasViaveis = reportIntent.getIntExtra("qtdeAuditoriasViaveis", 0);
+            int qtdeAuditoriasInviaveis = reportIntent.getIntExtra("qtdeAuditoriasInviaveis", 0);
+            String tempoMedioAuditorias = reportIntent.getStringExtra("tempoMedioAuditorias");
+            String observacao = reportIntent.getStringExtra("observacao");
+
+            String message = "Serviço " + idServico + " do contrato " + idContrato + " finalizado." +
+                    "\nAuditorias realizadas com sucesso: " + qtdeAuditoriasViaveis + " de " + qtdeAuditorias +
+                    " (" + qtdeAuditoriasInviaveis + " inviáveis)." +
+                    "\nTempo médio de captura: " + tempoMedioAuditorias +
+                    "\nObservação: " + observacao;
+
+            ((TextView) findViewById(R.id.report)).setText(message);
+        }
+
     }
 }
